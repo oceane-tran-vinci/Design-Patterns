d90b2fe54eb0f0e6f7a91d416139ae3ac5815801
oceane.tran <thuyduong.tran@student.vinci.be>
2025-09-27 19:02:09 +0200

    Fiche 1 - ex1 Observer

--- Fiche1/src/Main.java
+++ Fiche1/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     AnalyseurDeTexte analyseurDeTexte = new AnalyseurDeTexte(new ObsLignes(), new ObsMots(), new ObsPalindromes(), new ObsLignesContenantMot("Belgique"));
 
     try {
-      analyseurDeTexte.lireFichier(new File("test.txt"));
+      analyseurDeTexte.lireFichier(new File("testFiche1.txt"));
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
